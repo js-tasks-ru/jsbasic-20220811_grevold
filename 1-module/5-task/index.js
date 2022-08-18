e013e7f4e9c0f18ef0683eaae602a5ca6d7b74aa
Alexey Grevtsov <grevold@yandex.ru>
2022-08-18 18:21:44 +0300

    revert prev commit

--- 1-module/5-task/index.js
+++
@@ -1,6 +0,0 @@
-function truncate(str, maxlength) {
-  if (str.length <= maxlength) return str;
-  const newStr = [];
-  for (let i = 0; i < maxlength; i++) newStr.push(str[i]);
-  return newStr.join("") + "…";
-}
