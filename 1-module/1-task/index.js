c358eae4da2bed7f6702acbebdbc9ca71b7644de
Alexey Grevtsov <grevold@yandex.ru>
2022-08-22 09:03:18 +0300

    homework1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,6 +1,11 @@
 function factorial(n) {
-  if (n === 0) result = 1;
-  let result = 1;
-  for (let i = n; i >= 1; i--) result = result * i;
-  return result;
+  if (n === 0 || n === 1) {
+    return (result = 1);
+  } else {
+    let result = 1;
+    for (let i = n; i >= 1; i--) {
+      result = result * i;
+    }
+    return result;
+  }
 }
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,6 +1,6 @@
 function truncate(str, maxlength) {
   if (str.length <= maxlength) return str;
   const newStr = [];
-  for (let i = 0; i < maxlength; i++) newStr.push(str[i]);
+  for (let i = 0; i < maxlength-1; i++) newStr.push(str[i]);
   return newStr.join("") + "…";
 }
