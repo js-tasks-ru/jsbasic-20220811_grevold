85a62c56a3eae7809cd3d063666862360b046153
Alexey Grevtsov <grevold@yandex.ru>
2022-08-24 16:30:31 +0300

    Дз2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
 function sumSalary(salaries) {
-  // ваш код...
+  function isSalary(salary) {
+    if (
+      typeof salary === "number" &&
+      !isNaN(salary) &&
+      salary !== Infinity &&
+      salary !== -Infinity
+    ) {
+      return true;
+    }
+    return false;
+  }
+  let result = 0;
+  for (let key in salaries) {
+    if (isSalary(salaries[key])) {
+      result = result + salaries[key];
+    }
+  }
+  return result;
 }
