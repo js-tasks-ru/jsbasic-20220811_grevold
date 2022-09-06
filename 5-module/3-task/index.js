8dfcc025d55f0bf50f16c422f9333c1bb029d0ed
Alexey Grevtsov <grevold@yandex.ru>
2022-09-06 19:09:57 +0300

    hw5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let buttonNext = document.querySelector(".carousel__arrow_right");
+  let buttonPrev = document.querySelector(".carousel__arrow_left");
+  let screen = document.querySelector(".carousel__inner");
+  let widthScreen = screen.offsetWidth;
+
+  let state = 0;
+  buttonPrev.style.display = "none";
+
+  buttonNext.addEventListener("click", () => {
+    if (state > widthScreen) {
+      buttonNext.style.display = "none";
+    }
+    {
+      state = state + widthScreen;
+      screen.style.transform = `translateX(-${state}px)`;
+      buttonPrev.style.display = "";
+    }
+  });
+
+  buttonPrev.addEventListener("click", () => {
+    if (state === widthScreen) {
+      buttonPrev.style.display = "none";
+    }
+
+    {
+      state = state - widthScreen;
+      screen.style.transform = `translateX(-${state}px)`;
+      buttonNext.style.display = "";
+    }
+    console.log(widthScreen);
+    console.log(state);
+  });
 }
